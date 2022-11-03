ffa51f7c5409aec55950ca18d0fdfa7ba1211f9f
Aigul Mustafina <aimustafina2016@gmail.com>
2022-11-03 11:59:27 +0600

    Refactored conftest file

--- conftest.py
+++ conftest.py
@@ -2,12 +2,17 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function')
 def browser():
     print('\nstart browser...')
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     yield browser
     print('\nquit browser...')
     browser.quit()
